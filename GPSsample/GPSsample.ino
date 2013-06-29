643d045d558131fc8d2cf5fb19c30e61584a1036
Tao Qian <taoqian_2015@depauw.edu>
2013-06-28 21:45:41 -0400

    Renamed file.

--- GPSsample/GPSsample.ino
+++ GPSsample/GPSsample.ino
@@ -1,5 +1,7 @@
 #include <TinyGPS.h>
 
+byte gps_set_sucess = 0 ;
+
 /*
   6-12-12
   Aaron Weiss
@@ -51,6 +53,8 @@
 
 // Create an instance of the TinyGPS object
 TinyGPS gps;
+int navmode = 0;
+uint8_t buf[60]; 
 
 // This is where you declare prototypes for the functions that will be 
 // using the TinyGPS library.
@@ -72,7 +76,126 @@ void setup()
   Serial.println("Ensure GPS shield pin 2 connects to Ardunio MEGA pin 19");
   Serial.println("       ...waiting for lock...           ");
   Serial.println("");
+  
+  gps_check_nav();
+  //setFlightMode();
 }
+
+uint8_t gps_check_nav(void)
+{
+  uint8_t request[8] = {
+    0xB5, 0x62, 0x06, 0x24, 0x00, 0x00, 0x2A, 0x84                                                             };
+  sendUBX(request, 8);
+
+  // Get the message back from the GPS
+  gps_get_data();
+  // Return the navigation mode and let the caller analyse it
+  navmode = buf[8];
+  Serial.print("Mode: ");
+  Serial.println(navmode);//6 is flight mode, 3 is normal mode.
+}
+
+void gps_get_data()
+{
+  Serial1.flush();
+  // Clear buf[i]
+  for(int i = 0;i<60;i++) 
+  {
+    buf[i] = 0; // clearing buffer  
+  }  
+  int i = 0;
+  unsigned long startTime = millis();
+
+  while ((i<60) && ((millis() - startTime) < 1000) ) { 
+    if (Serial1.available()) {
+      buf[i] = Serial1.read();
+      i++;
+    }
+  }
+}
+
+void setFlightMode()
+{
+  // THIS COMMAND SETS FLIGHT MODE AND CONFIRMS IT 
+  Serial.println("Setting uBlox nav mode: ");
+  uint8_t setNav[] = {
+    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC                      };
+  while(!gps_set_sucess)
+  {
+    sendUBX(setNav, sizeof(setNav)/sizeof(uint8_t));
+    gps_set_sucess=getUBX_ACK(setNav);
+  }
+  gps_set_sucess=0;
+}
+
+// Send a byte array of UBX protocol to the GPS
+void sendUBX(uint8_t *MSG, uint8_t len) {
+  Serial.print("Sent:");
+  for(int i=0; i<len; i++) {
+    Serial1.write(MSG[i]);
+    Serial.print(MSG[i], HEX);
+  }
+  Serial.println();
+}
+
+// Calculate expected UBX ACK packet and parse UBX response from GPS
+boolean getUBX_ACK(uint8_t *MSG) {
+  uint8_t b;
+  uint8_t ackByteID = 0;
+  uint8_t ackPacket[10];
+  unsigned long startTime = millis();
+  Serial.print(" * Reading ACK response: ");
+ 
+  // Construct the expected ACK packet    
+  ackPacket[0] = 0xB5;	// header
+  ackPacket[1] = 0x62;	// header
+  ackPacket[2] = 0x05;	// class
+  ackPacket[3] = 0x01;	// id
+  ackPacket[4] = 0x02;	// length
+  ackPacket[5] = 0x00;
+  ackPacket[6] = MSG[2];	// ACK class
+  ackPacket[7] = MSG[3];	// ACK id
+  ackPacket[8] = 0;		// CK_A
+  ackPacket[9] = 0;		// CK_B
+ 
+  // Calculate the checksums
+  for (uint8_t i=2; i<8; i++) {
+    ackPacket[8] = ackPacket[8] + ackPacket[i];
+    ackPacket[9] = ackPacket[9] + ackPacket[8];
+  }
+ 
+  while (1) {
+ 
+    // Test for success
+    if (ackByteID > 9) {
+      // All packets in order!
+      Serial.println(" (SUCCESS!)");
+      return true;
+    }
+ 
+    // Timeout if no valid response in 3 seconds
+    if (millis() - startTime > 3000) { 
+      Serial.println(" (FAILED!)");
+      return false;
+    }
+ 
+    // Make sure data is available to read
+    if (Serial1.available()) {
+      b = Serial1.read();
+ 
+      // Check that bytes arrive in sequence as per expected ACK packet
+      if (b == ackPacket[ackByteID]) { 
+        ackByteID++;
+        Serial.print(b, HEX);
+      } 
+      else {
+        ackByteID = 0;	// Reset and look again, invalid order
+      }
+ 
+    }
+  }
+}
+ 
 
 // This is the main loop of the code. All it does is check for data on 
 // the RX pin of the ardiuno, makes sure the data is valid NMEA sentences, 
@@ -83,12 +206,12 @@ void loop()
   {
       char c = Serial1.read();    // load the data into a variable...
       //Serial.print("C is: ");
-      //Serial.print(c);
+      Serial.print(c);
       //Serial.println(".");
-      if(gps.encode(c))      // if there is a new valid sentence...
-      {
-        getgps(gps);         // then grab the data.
-      }
+      //if(gps.encode(c))      // if there is a new valid sentence...
+      //{
+      //  getgps(gps);         // then grab the data.
+      //}
   }
 }
 
@@ -109,6 +232,8 @@ void getgps(TinyGPS &gps)
   Serial.print(latitude,5); 
   Serial.print(", "); 
   Serial.println(longitude,5);
+  
+  
   
   // Same goes for date and time
   int year;
